a9700e57d5868c234b67ea70f15754728d592b8d
bit1129 <bit1129@163.com>
2016-02-02 17:37:58 +0800

    Spark SQL Test to test that Spark SQL can work

---
+++ sql/core/src/main/scala/org/apache/spark/sql/test/SparkSQLTest.scala
@@ -0,0 +1,18 @@
+package org.apache.spark.sql.test
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkContext, SparkConf}
+
+
+object SparkSQLTest {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("SparkSQLTest").setMaster("local");
+    val sc = new SparkContext(conf);
+    val ssc = new SQLContext(sc);
+    val path = "D:/opensourceprojects/spark20160202/examples/src/main/resources/users.parquet"
+    ssc.read.parquet(path).registerTempTable("TBL_USER");
+    val df = ssc.sql("select * from TBL_USER");
+    df.show(20)
+    sc.stop
+  }
+}
